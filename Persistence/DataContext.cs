4baea93534e06056d954702e15ea6cd15593c3d8
Mohamed Derhalli <derhallim@gmail.com>
2020-03-01 14:35:14 -0500

    Implementing first migration for DB

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     public class DataContext: DbContext
     {
+        public DataContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Apartment> Apartments { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Bingo1;Integrated Security=True");
+        }
     }
 }
