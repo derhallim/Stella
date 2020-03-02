ec1c560f3b0a878071a5f6815009381343fcc628
Mohamed Derhalli <derhallim@gmail.com>
2020-03-01 21:53:41 -0500

    modifying apartments controller to add attributes

--- API/Controllers/ApartmentsController.cs
+++ API/Controllers/ApartmentsController.cs
@@ -1,25 +1,42 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApartmentsController
+    public class ApartmentsController: ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(){
-            return new string[] { "value1", "value2"};
+        private readonly DataContext _context;
+        public ApartmentsController(DataContext context)
+        {
+            _context = context;
+
         }
 
         [HttpGet]
-        public ActionResult<string> Get(int id){
-            return "value";
+        public async Task<ActionResult<IEnumerable<Apartment>>> Get()
+        {
+            var apartments = await _context.Apartments.ToListAsync();
+            return Ok(apartments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Apartment>> Get(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+            return Ok(apartment);
         }
 
         [HttpPost]
-        public void Post([FromBody] string value){
-            
+        public void Post([FromBody] string value)
+        {
+
         }
     }
 }
---
+++ API/Controllers/CitiesController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> Get(){
+            var cities = await _context.Cities.ToListAsync();
+            return Ok(cities);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<City>> GetAction(int id){
+            var city = await _context.Cities.FindAsync(id);
+            return Ok(city);
+        }
+
+    }
+}
