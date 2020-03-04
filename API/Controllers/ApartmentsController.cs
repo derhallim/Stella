11f401e9f67764a8bc0797bdace06de4b66c1b19
Mohamed Derhalli <derhallim@gmail.com>
2020-03-04 18:58:36 -0500

    Add create apartment logic

--- API/Controllers/ApartmentsController.cs
+++ API/Controllers/ApartmentsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,10 +29,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Apartment>> Get(int id)
+        public async Task<ActionResult<Apartment>> Details(Guid id)
         {
-            var apartment = await _context.Apartments.FindAsync(id);
-            return Ok(apartment);
+           return await _mediator.Send(new Details.Query{Id= id});
         }
 
         [HttpPost]
---
+++ Application/Apartments/Create.cs
@@ -0,0 +1,13 @@
+using MediatR;
+
+namespace Application.Apartments
+{
+    public class Create
+    {
+        public class Command: IRequest{
+
+        }
+
+        
+    }
+}
