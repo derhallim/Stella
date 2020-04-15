2db03df9839bdecadced819a3eabbf6cc490dbad
Mohamed Derhalli <derhallim@gmail.com>
2020-04-14 21:26:36 -0400

    Changing target framework for projects, and adding login

---
+++ Application/User/Login.cs
@@ -0,0 +1,62 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Net;
+
+namespace Application.User
+{
+    public class Login
+    {
+        public class Query : IRequest<AppUser>
+        {
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+        }
+
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, AppUser>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            {
+                _signInManager = signInManager;
+                _userManager = userManager;
+
+            }
+
+            public async Task<AppUser> Handle(Query request, CancellationToken canellationToken)
+            {
+                    var user = await _userManager.FindByEmailAsync(request.Email);
+                    
+                    if(user == null){
+                        throw new Exception(HttpStatusCode.Unauthorized.ToString());
+                    }
+
+                    var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                    if(result.Succeeded){
+                        //TODO: generate token
+                        return user;
+                    }
+                    
+                    throw new Exception(HttpStatusCode.Unauthorized.ToString());
+            }
+        }
+    }
+}
